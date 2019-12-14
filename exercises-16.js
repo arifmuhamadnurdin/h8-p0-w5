18c7c70262f23a07469532fcd6b36cad669969e4
arifmuhamadnurdin <arifmuhamadnurdin25@gmail.com>
2019-12-14 23:44:39 +0700

    update

---
+++ exercises-16.js
@@ -0,0 +1,15 @@
+function totalDigitRekursif(angka) {
+var angkastring = angka.toString();
+var hasilstring = 0;
+for (var i=0; i < angkastring.length;i++){
+  hasilstring +=  parseInt(angkastring[i]);  
+}
+return hasilstring;
+}
+
+// TEST CASES
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
